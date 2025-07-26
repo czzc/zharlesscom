b62be9495994e59ac2be94a1264b7d1b7fa028c0
Zac <zlh309@gmail.com>
2025-07-26 11:46:18 -0400

    testing dev subdomain deployment

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -3,7 +3,7 @@ export const personalInfo = {
   bio: [
     { one: "Hey, I'm a full-stack developer based out of southwestern Virginia, who lives somewhere between clean design and chaotic late-night debugging sessions. I've worked across the stack—building interfaces, wiring up APIs, and coaxing backend services into doing what they're supposed to." },
     { two: "I like building things that are actually useful—and don't break when you look at them funny. Whether it's crafting clean code, untangling spaghetti logic, or pretending semicolons are optional, I'm here for it." },
-    { three: "Always learning, always improving—and occasionally Googling the same error for the 48th time."}
+    { three: "Always learning, always improving—and occasionally Googling the same error for the 49th time."}
   ],
   avatar: "./me.webp",
   logo: "./logo.png",
