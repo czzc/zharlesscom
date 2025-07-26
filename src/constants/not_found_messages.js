2efe10badc6b4e4b1077978b3492f723cd1fc73e
Zac <zlh309@gmail.com>
2025-07-26 12:31:56 -0400

    custom 404 page

---
+++ src/constants/not_found_messages.js
@@ -0,0 +1,10 @@
+export const not_found_messages = [
+    "Well, this is awkward.",
+    "¯\\_(ツ)_/¯",
+    "There is no page, only Zuul.",
+    "It's not you, it's the URL.",
+    "Somewhere, I just cried a single tear because you hit this page.",
+    "404: Not Found. But you already knew that.",
+    "This page is as lost as my motivation on a Monday morning.",
+    "You’ve stumbled into Praetorian’s territory. We hope you have a backup of your identity."
+]
